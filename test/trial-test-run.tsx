326e8a236b3e570c04f223445d1841d962481f7d
JuanPabloZ <juanpablo.zunigah@gmail.com>
2023-01-22 13:33:15 +0100

    test

--- test/trial-test-run.tsx
+++ test/trial-test-run.tsx
@@ -19,20 +19,22 @@ console.log(randomArraylevel3);
 maxProfit(randomArraylevel1);
 
 
-function maxProfit( arr : number[]): {profit: number, buyDay: number, sellDay: number} {
+function maxProfit(prices: number[]): {profit: number, buyDay: number, sellDay: number} {
+    let minPrice = prices[0];
     let maxProfit = 0;
     let buyDay = 0;
     let sellDay = 0;
-    let minPrice = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < minPrice) {
-            minPrice = arr[i];
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
             buyDay = i;
-        }
-        let currentProfit = arr[i] - minPrice;
-        if (currentProfit > maxProfit) {
-            maxProfit = currentProfit;
-            sellDay = i;
+        } else {
+            let profit = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+                sellDay = i;
+            }
         }
     }
     console.log("the max profit is achieved" + maxProfit)
@@ -43,6 +45,7 @@ function maxProfit( arr : number[]): {profit: number, buyDay: number, sellDay: n
 
 // To learn more about the language, click above in "Examples" or "What's New".
 // Otherwise, get started by removing these comments and the world is your playground.
+  
   
 // You can find the answer in console here
 //https://www.typescriptlang.org/play?#code/PTAEHUFMBsGMHsC2lQBd5oBYoCoE8AHSAZVgCcBLA1UABWgEM8BzM+AVwDsATAGiwoBnUENANQAd0gAjQRVSQAUCEmYKsTKGYUAbpGF4OY0BoadYKdJMoL+gzAzIoz3UNEiPOofEVKVqAHSKymAAmkYI7NCuqGqcANag8ABmIjQUXrFOKBJMggBcISGgoAC0oACCbvCwDKgU8JkY7p7ehCTkVDQS2E6gnPCxGcwmZqDSTgzxxWWVoASMFmgYkAAeRJTInN3ymj4d-jSCeNsMq-wuoPaOltigAKoASgAywhK7SbGQZIIz5VWCFzSeCrZagNYbChbHaxUDcCjJZLfSDbExIAgUdxkUBIursJzCFJtXydajBBCcQQ0MwAUVWDEQC0gADVHBQGNJ3KAALygABExAA8gAxHDgCqPGmgYgAaQAks9nqAACJy5WgABygpwEEFjxlfMUFME8HcAWg8GYAApafTGe5WZQOe4AJSKdw0MguJAVMhevDuPTQACMPMqfqYAWSbEQVoA3u5OMxYvlQAAOAC+-CtLp5AD5QABZOqYKMW+BkK3F2IBL08JA50AAKlAACYAAy5gDUoGDLoA3O7IJ7vYhff7AzBW2Hx5How2Eyjk5hUx2s6BG9yC9XS8ly5Wd7XR42Wx3u72B0OR-WxxGA5AgwBmGd3qMx+OJ5epgAs7fXm+3Esy3gCsqyAutuAbXNT07UAez7QdjVNSBzUtK0IJ9O9JxDS8kLNC1rQw28JwfKdcMaE18LQojZ3vJ9L0URAzloNhknkdDR1o7CEOCRRki4WB6kaUAmNWFiUnYsQ-VAVNOHYRBpG+ABtABdF1UzjAhWPkWT5MUsh+Gkdg8GVJhdIU747BgaBTLwcz9IzUA40UEoSg9ETmO0mheXbQdXLcYdxmM2yw18lzXPcwRrJCny-IiwLEAyFj1BQXlHDIJT2xUuKSmSCsN3cigw2DfsRFAAAeKSyHNJdYlKiguy7XNnP8-yEQ3dKlIoFSKpEpLKAsZrwta1rEs4ZKljSv0uuy4aRtcoyTKYMMKBy1qMzm-z3NgfEnG2cS2O8qqZtmMaJsgNa2tSK0dr9FFUAO+RQALUTHtQIb5tGzyJKO269oerzLvmqLoBs5beVWzbXI2-yYdcvCUIIq0+S+DzQS0n6RGEBgNAoUjuD5OC0bet1-IR1DrRRu5FrhZahEJntFts0n4Yo5CKeR1GaZB6BhlpvAsYZq5oqYFmSicVB8S8TSvNTV6vMM4KzKCpa8Cs0HbNTHnbIzQcNuKHBmg8MgvEQCtnGBdgaFRxgk3YBhmEgfhYF52BEg5eA9BELw+TpBkmUEQn8r5cAHFQAByYQNUgCQ+SCFRBS+Mh3ii-hHaOVBHAUVxpAFpwzZ0PmviitFEGhbGeCwHIK2iLHQEMfF5kYFg2C4bgghKIA
